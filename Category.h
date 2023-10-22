74f20aed99cec9b393ed5fa616bb6ace091e5657
Claira27 <125986168+Claira27@users.noreply.github.com>
2023-10-22 10:19:39 +0700

    Update Category.h

--- Category.h
+++ Category.h
@@ -1,31 +1,38 @@
-#include"Date.h"
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<string>
+#include<map>
 using namespace std;
 
 #ifndef CATEGORY_H
 #define CATEGORY_H
-struct Description
-{
-    string brand;
-    string origin;
-    string type;// daily dress complicated pilot field dive racing travel luxury other
-};
 class Category
 {
+    //Type and sex
     string ID;
     string name;
-    Description general;
 public:
+    static map<string, Category> categoryList;
+    static int numberOfCategory;
     Category();
-    Category(string ID, string name, Description general);
+    Category(const string &ID, const string &name);
     //Get information
     string getID() const;
     string getName() const;
-    Description getGeneral() const;
     //Mutator functions
     void setID(const string &ID);
     void setName(const string &name);
-    void setGeneral(const Description &general);
+    //Functions for CRUD
     friend istream &operator>>(istream &is, Category &myCategory);
     friend ostream &operator<<(ostream &os, const Category &myCategory);
+    static void CategoryLoad();
+    static int getNumberOfCategory();
+    static void ReadCategory();
+    static void FindCategory();
+    static void CreateCategory();
+    static int ReferenceConstraint(string ID);
+    static void EraseCategory();
+    static void UpdateCategory();
 };
 #endif // CATEGORY_H
