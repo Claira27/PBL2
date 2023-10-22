4872b97c765168a496f3646f3c70fd45f6b43de4
Claira27 <125986168+Claira27@users.noreply.github.com>
2023-10-22 10:20:00 +0700

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -1,27 +1,40 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<string>
+#include<map>
 using namespace std;
 
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 class Customer
 {
-    static int numberOfCustomer;
-    string ID;
-    string name;
     string phone;
+    string name;
 public:
+    friend class Order;
+    static int numberOfCustomer;
+    static map<string, Customer> customerList;
     Customer();
-    Customer(string ID,const string &name,const string &phone);
+    Customer(const string &phone, const string &name);
     //Get information
-    string getID() const;
     string getName() const;
     string getPhone() const;
     //Mutator functions
-    void setID(string ID);
-    void setName(const string &name);
-    void setPhone(const string &phone);
+    void setName(string name);
+    void setPhone(string phone);
+    //Functions for CRUD
     friend istream &operator>>(istream &is, Customer &myCustomer);
     friend ostream &operator<<(ostream &os, const Customer &myCustomer);
+    static void CustomerLoad();
+    static int getNumberOfCustomer();
+    static void ReadCustomer();
+    static void FindCustomer();
+    static void CreateCustomerList();
+    static void createNewCustomer(const string &phone);//using when new order with new customer
+    static int ReferenceConstraint(const string &phone);
+    static void EraseCustomer();
+    static void UpdateCustomer();
 };
 
 #endif // CUSTOMER_H
