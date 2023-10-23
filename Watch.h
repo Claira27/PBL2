00e2077afb54ef6b1d9c56b73c61c2100e6a2af8
Claira27 <125986168+Claira27@users.noreply.github.com>
2023-10-23 07:41:43 +0700

    Update Watch.h

--- Watch.h
+++ Watch.h
@@ -1,12 +1,22 @@
-#include<iostream>
-#include<fstream>
-#include<cstring>
-#include<map>
-#include<ctime>
-using namespace std;
-
 #ifndef WATCH_H
 #define WATCH_H
+#include<iostream>
+#include<conio.h>
+#include<fstream>
+#include<cstring>
+#include<ctime>
+#include<iomanip>
+#include<sstream>
+#include<iomanip>
+#include<cstdlib> // For system("cls")
+#include<unistd.h> // For sleep (sleep function on Unix-like systems)
+#include<windows.h>
+#include<vector>
+#include<map>
+#include<set>
+using namespace std;
+
+class OrderDetail;
 class Watch
 {
     string ID;
@@ -36,12 +46,11 @@ public:
     friend ostream &operator<<(ostream &os, const Watch &myWatch);
     static void WatchLoad();
     static int getNumberOfWatch();
-    static void ReadWatch();
-    static void FindWatch();
-    static void CreateWatch();
+    static void ReadWatch(const string &productID);
+    static void CreateWatch(const string &productID);
     static int ReferenceConstraint(const string& ID);
-    static void EraseWatch();
-    static void UpdateWatch();
+    static void EraseWatch(const string &productID);
+    static void UpdateWatch(const string &productID);
 };
 
 #endif // CUSTOMER_H
