f5dda27a9d2a8a3f0069926ee7384e0a685310d7
Claira27 <125986168+Claira27@users.noreply.github.com>
2023-10-23 07:39:31 +0700

    Update Order.h

--- Order.h
+++ Order.h
@@ -1,15 +1,25 @@
-#include<iostream>
-#include<cstring>
-#include<map>
-#include<ctime>
-#include<iomanip>
-#include<sstream>
-
-using namespace std;
-
 #ifndef ORDER_H
 #define ORDER_H
 
+#include<iostream>
+#include<conio.h>
+#include<fstream>
+#include<cstring>
+#include<ctime>
+#include<iomanip>
+#include<sstream>
+#include<iomanip>
+#include<cstdlib> // For system("cls")
+#include<unistd.h> // For sleep (sleep function on Unix-like systems)
+#include<windows.h>
+#include<vector>
+#include<map>
+#include<set>
+
+using namespace std;
+
+class Watch;
+class Customer;
 class Order
 {
     string ID;
@@ -34,7 +44,7 @@ public:
     void setAmountDue();
     //Functions for CRUD
     friend istream &operator>>(istream &is, Order &myOrder);
-    friend ostream &operator<<(ostream &os, const Order &myOrder);
+    friend ostream &operator<<(ostream &os, Order &myOrder);
     static void OrderLoad();
     static int getNumberOfOrder();
     static string generateOrderID(const string &myid);
@@ -45,5 +55,6 @@ public:
     static void EraseOrder();
     static void DeleteOrder();
     static void UpdateOrder();
+    static void DeleteSale();
 };
 #endif // ORDER_H
