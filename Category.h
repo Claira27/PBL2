8e9f282c5c887e9f9a1097a27a504560e9d57ad7
Claira27 <125986168+Claira27@users.noreply.github.com>
2023-10-23 07:38:50 +0700

    Update Category.h

--- Category.h
+++ Category.h
@@ -1,12 +1,31 @@
-#include<iostream>
-#include<fstream>
-#include<cstring>
-#include<string>
-#include<map>
-using namespace std;
-
 #ifndef CATEGORY_H
 #define CATEGORY_H
+
+#include<iostream>
+#include<conio.h>
+#include<fstream>
+#include<cstring>
+#include<ctime>
+#include<iomanip>
+#include<sstream>
+#include<iomanip>
+#include<cstdlib> // For system("cls")
+#include<unistd.h> // For sleep (sleep function on Unix-like systems)
+#include<windows.h>
+#include<vector>
+#include<map>
+#include<set>
+using namespace std;
+// ANSI color codes for text
+const string ANSI_RESET = "\033[0m";
+const string ANSI_RED = "\033[31m";
+const string ANSI_GREEN = "\033[32m";
+const string ANSI_YELLOW = "\033[33m";
+const string ANSI_CYAN = "\033[36m";
+const string ANSI_BOLD = "\033[1m";
+const string ANSI_BLINK = "\033[5m";
+
+class Product;
 class Category
 {
     //Type and sex
