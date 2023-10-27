78fd6c7f13dffd815a99e6629c93eb2b3ac298d5
Claira27 <125986168+Claira27@users.noreply.github.com>
2023-10-27 19:06:28 +0700

    Update OrderDetail.h

--- OrderDetail.h
+++ OrderDetail.h
@@ -22,40 +22,32 @@ struct Warranty
     string coverage; //Battery Replacement: Some warranties for watches may cover the replacement of the battery, strap or bracelet within a specified time frame.
 };
 class Order;
-class Product;
 class OrderDetail
 {
     string ID;
     string orderID;
     string IDserie;
     Warranty warranty;
-
-public:
-    friend class Watch;
-    friend class Order;
+protected:
     static int sales;// the number of watches sold out
     static map<string,OrderDetail> saleList;
+public:
     OrderDetail();
     //Get information
     string getID() const;
     string getOrderID() const;
     string getIDSerie() const;
-    Warranty getWarranty() const;
+    Warranty getWarranty();
     //mutator function
     void setID();
     void setOrderID(const string &orderID);
     void setIDSerie(const string &IDserie);
-    void setWarranty();
+    void setWarranty(Warranty warrant);
     //Functions for CRUD
     friend istream &operator>>(istream &is, OrderDetail &myOrderDetail);
     friend ostream &operator<<(ostream &os, const OrderDetail &myOrderDetail);
     static void SaleLoad();
     static int getSales();
-    static void ReadSale();
-    static void FindSale();
-    static void CreateSale(const string &orderID);
-    static void EraseSale(const string &orderID);
-    static void DeleteSale();
-    static void UpdateSale(const string &orderID);
+
 };
 #endif // ORDERDETAIL_H
